1f19d65629b4cdc48d11041cdfd7fe4c1b83e646
clever matchless bravo villafuerte <cleveresclip@gmail.com>
2021-01-13 10:38:03 -0400

    cin

---
+++ programa3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{//1.-Inicio
+    int h,a,b,area;
+    cin>>h>>a>>b;//2.-Leer h,a,b
+    area=b*a/2;//3.-area=b*a/2
+    cout<<area;//4.-mostrar area
+    return 0;
+}//5.-Fin
+
+
+
+
